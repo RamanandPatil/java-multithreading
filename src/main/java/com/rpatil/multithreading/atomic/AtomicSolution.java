c631458049edf6c64ca58ca126a550c2be36c9c3
RamanandPatil <ramanand.patil@gmail.com>
2022-08-31 20:46:18 +0530

    Added comments and improved code by renaming and refactoring

--- src/main/java/com/rpatil/multithreading/atomic/AtomicSolution.java
+++ src/main/java/com/rpatil/multithreading/atomic/AtomicSolution.java
@@ -4,22 +4,34 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 public class AtomicSolution {
     public static void main(String[] args) {
-        AtomicTask atomicTask = new AtomicTask();
+        // Common counterTask which will be shared between 2 or more threads
+        AtomicTask atomicCounterTask = new AtomicTask();
 
-        Thread t1 = new Thread(atomicTask);
-        Thread t2 = new Thread(atomicTask);
+        // Create threads with shared atomicCounterTask
+        Thread thread1 = new Thread(atomicCounterTask);
+        Thread thread2 = new Thread(atomicCounterTask);
 
-        t1.start();
-        t2.start();
+        // Start the threads
+        thread1.start();
+        thread2.start();
 
+        // Wait for all the threads to complete there execution
         try {
-            t1.join();
-            t2.join();
+            thread1.join();
+            thread2.join();
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
 
-        System.out.println("Count is: " + atomicTask.getCount());
+        // Since both threads thread1 and thread2 were working on a same atomicCounterTask
+        // (i.e. shared resource) it is expected that the counter value should be 100.
+        // Here,this happens always because counter uses atomic operation using AtomicInteger.
+        if (atomicCounterTask.getCount() != 100) {
+            System.err.print("Incorrect result, count value should be 100, but ");
+            System.err.println("Count value is: " + atomicCounterTask.getCount());
+        } else {
+            System.out.println("Correct result, count value is 100");
+        }
     }
 }
 
@@ -35,7 +47,7 @@ class AtomicTask implements Runnable {
     public void run() {
         for (int i = 1; i <= 50; i++) {
             try {
-                Thread.sleep(50);
+                Thread.sleep(10);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
--- src/main/java/com/rpatil/multithreading/atomic/NonAtomicProblem.java
+++ src/main/java/com/rpatil/multithreading/atomic/NonAtomicProblem.java
@@ -2,26 +2,38 @@ package com.rpatil.multithreading.atomic;
 
 public class NonAtomicProblem {
     public static void main(String[] args) {
-        Task task = new Task();
+        // Common counterTask which will be shared between 2 or more threads
+        CounterTask counterTask = new CounterTask();
 
-        Thread t1 = new Thread(task);
-        Thread t2 = new Thread(task);
+        // Create threads with shared counterTask
+        Thread thread1 = new Thread(counterTask);
+        Thread thread2 = new Thread(counterTask);
 
-        t1.start();
-        t2.start();
+        // Start the threads
+        thread1.start();
+        thread2.start();
 
+        // Wait for all the threads to complete there execution
         try {
-            t1.join();
-            t2.join();
+            thread1.join();
+            thread2.join();
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
 
-        System.out.println("Count is: " + task.getCount());
+        // Since both threads thread1 and thread2 were working on a same counterTask
+        // (i.e. shared resource) it is expected that the counter value should be 100.
+        // But this happens very rarely because of the concurrency issue, we always get less value.
+        if (counterTask.getCount() != 100) {
+            System.err.print("Incorrect result, count value should be 100, but ");
+            System.err.println("Count value is: " + counterTask.getCount());
+        } else {
+            System.out.println("Correct result, count value is 100");
+        }
     }
 }
 
-class Task implements Runnable {
+class CounterTask implements Runnable {
 
     private int count;
 
@@ -33,7 +45,7 @@ class Task implements Runnable {
     public void run() {
         for (int i = 1; i <= 50; i++) {
             try {
-                Thread.sleep(50);
+                Thread.sleep(10);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
